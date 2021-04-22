f041425e7a04099182170a514efa5172de079375
Sebastian Zambrano <Sebastianrza@gmail.com>
2021-04-22 22:00:41 +0200

    actualizacion

--- Practica3/foro.php
+++ Practica3/foro.php
@@ -7,7 +7,7 @@
     <title>Foro</title>
     <link rel="icon" type="image/png" href="./img/favicon.png">
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    <link rel="stylesheet" type="text/css" href="css/foro.css">
+    <link rel="stylesheet" type="text/css" href="./css/foro.css">
 </head>
 <body>
 	<?php
--- Practica3/perfil.php
+++ Practica3/perfil.php
@@ -1,6 +1,9 @@
 <?php
 require_once __DIR__.'/APP/BD/database.php';
 session_start();
+if(!isset($_SESSION["login"])){
+    header('Location: index.php');
+}
 ?>
 
 <!DOCTYPE html>
