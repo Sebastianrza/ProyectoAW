6294ebbf919ea340576b41c051af6b7ebd17acfe
Sebastian Zambrano <45112547+Sebastianrza@users.noreply.github.com>
2021-05-03 15:55:31 +0200

    Faltaba }

---
+++ Práctica3.1/includes/lista.php
@@ -0,0 +1,34 @@
+<?php
+	require_once '../MODELO/conexion.php';
+	
+	class lista extends reproductor {
+		var $genero;
+		var $numPodcast;
+		var $activa;
+
+		public function getNombre(){ return $this->genero; }
+		public function setNombre($genero){ $this->genero = $genero; }
+
+		public function __construct($genero, $numPodcast)
+		{	
+			$this->genero=$genero;			 			
+			$this->numPodcast=$numPodcast;		
+		}
+		public function crearLista($genero)
+		{
+			$con = new Conexion();
+			$con->Conectar();
+			$activa="true";
+
+
+			$this->genero=$genero;		
+			$this->activa=$activa;
+			 
+			$query="INSERT INTO LISTA VALUES(NULL, :genero, :activa)";
+	    	$consulta=$con->conexion->prepare($query);
+	        $consulta->bindValue(':genero', $this->genero);	
+	        $consulta->bindValue(':activa', $this->activa);	
+	        $consulta->execute();	      
+	  	   return true; 
+		}	
+	}
