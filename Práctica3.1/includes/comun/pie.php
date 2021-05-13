8cc9c419375a1b3159da00a52eaf341dccc2b132
Sebastian Zambrano <sebastianrza@gmail.com>
2021-05-13 17:42:49 +0200

    ...

--- Práctica3.1/includes/comun/pie.php
+++ Práctica3.1/includes/comun/pie.php
@@ -1,6 +1,6 @@
 <footer class='foot'>
         <p>Síguenos</p>
-        <a href="" target="_blank"><img class='icon' src="./img/icon-twitter.png" alt=""></a>
-        <a href="" target="_blank"><img class='icon'src="./img/icon-facebook.png" alt=""></a>
+        <a href=""><img class='icon' src="./img/icon-twitter.png" alt=""></a>
+        <a href=""><img class='icon'src="./img/icon-facebook.png" alt=""></a>
 		<a href="#" class="scrollToTop" style="display: block;"><img src="./img/icon-arrow-top.png" alt="">
 </footer>
