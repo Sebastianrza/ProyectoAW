b6d392faa53a862a836a49e981b7d8942fba46f5
Sebastian Zambrano <sebastianrza@gmail.com>
2021-05-13 17:32:12 +0200

    ...

--- Práctica3.1/includes/comun/pie.php
+++ Práctica3.1/includes/comun/pie.php
@@ -1,3 +1,6 @@
-<footer>
-	Pie de página
+<footer class='foot'>
+        <p>Síguenos</p>
+        <a href="" target="_blank"><img class='icon' src="./img/icon-twitter.png" alt=""></a>
+        <a href="" target="_blank"><img class='icon'src="./img/icon-facebook.png" alt=""></a>
+		<a href="#" class="scrollToTop" style="display: block;"><img src="./img/icon-arrow-top.png" alt="">
 </footer>
