0b9bd109c80ab92a00aee8bbe5e4322acfb0b58a
Sebastian Zambrano <sebastianrza@gmail.com>
2021-05-13 18:12:06 +0200

    agrupación de html duplicado

--- Práctica3.1/includes/comun/cabecera.php
+++ Práctica3.1/includes/comun/cabecera.php
@@ -9,10 +9,36 @@ function mostrarSaludo() {
 }
 ?>
 <header>
-	<h1></h1>
-	<div class="saludo">
-	<?php
-		mostrarSaludo();
-	?>
+	<div class = "navbar">
+		<nav class = "navigation hide" id = "navigation">
+					<span class = "close-icon" onclick = "showIconBar()"><i class = "fa fa-close"></i></span>
+			<ul class = "navlist">
+				<li class = "nav-item">
+					<a href = "index.php">Index</a>
+				</li>		
+				<li class = "nav-item">
+					<a href = "reproductor.php">Reproductor</a>
+				</li>	
+				<li class = "nav-item">
+					<a href = "foro.php">Foro</a>
+				</li>
+				<li class = "nav-item">
+					<a href = "playlist.php">Explora Playlist</a>
+				</li>		
+				<li class = "nav-item">
+					<a href = "perfil.php">Perfil</a>
+				</li>
+						
+			</ul>
+		</nav>
+		<a href = "#" class = "bar-icon" id = "iconBar" onclick = "hideIconBar()"><i class="fa fa-bars"></i></a>
+		<h1 class = "WaveCast-logo"> WaveCast </h1>
+		<h1></h1>
+		<div class="saludo">
+			<?php
+				mostrarSaludo();
+			?>
+		</div>
 	</div>
+	
 </header>
--- Práctica3.1/index.php
+++ Práctica3.1/index.php
@@ -3,7 +3,6 @@
 require_once __DIR__.'/includes/config.php';
 
 $tituloPagina = 'Portada';
-
 $contenidoPrincipal = <<<EOS
 <h1>Página principal</h1>
 <p> Aquí está el contenido público, visible para todos los usuarios. </p>
