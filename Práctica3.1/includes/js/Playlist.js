88e30e59411ac1bbe6e4b11041fab29474dbbcc5
Colevo <algonz22@ucm.es>
2021-05-28 20:02:21 +0200

    Pequeños comentarios

--- Práctica3.1/includes/js/Playlist.js
+++ Práctica3.1/includes/js/Playlist.js
@@ -1,8 +1,10 @@
 $(document).ready(function () {
 
   $("audio").on("ended", function (event) {
-
+    //guardamos el enlace del elemento pod que son los audio 
     let nextaudios = $("#pod").prop("href");
+    //redireccionamos a la variable almacenada arriba
+    //PROBLEMA: $pod sólo devuelve el primero, eso tenemos que arreglarlo para eliminar los archivos que ya se han escuchado
     window.location.href = nextaudios;
 
   });
