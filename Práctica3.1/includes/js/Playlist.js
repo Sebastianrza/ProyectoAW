c601d8eb3b4056128a4a1a4d826a04c5045be417
Colevo <algonz22@ucm.es>
2021-05-28 21:01:00 +0200

    Intento colas

--- Práctica3.1/includes/js/Playlist.js
+++ Práctica3.1/includes/js/Playlist.js
@@ -1,12 +1,21 @@
+var contador = -1;
 $(document).ready(function () {
 
   $("audio").on("ended", function (event) {
+    var audios= [];
+    $(".contenedorPlaylist > .infoPlaylist > a").each(function(){
+      //Almacenar cada uno en un array
+      audios.push($(this).prop("href"));
+    });
     //guardamos el enlace del elemento pod que son los audio 
-    let nextaudios = $("#pod").prop("href");
     //redireccionamos a la variable almacenada arriba
     //PROBLEMA: $pod sólo devuelve el primero, eso tenemos que arreglarlo para eliminar los archivos que ya se han escuchado
-    window.location.href = nextaudios;
-
+    contador++;
+    if(contador > audios.length-1){
+     contador = 0; 
+    }
+    window.location.href = audios[contador];
+  
   });
 
 });
