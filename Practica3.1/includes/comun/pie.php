4f66338675e7f405a1f3aff4c028569012190c44
Sebastian Zambrano <45112547+Sebastianrza@users.noreply.github.com>
2021-07-14 11:53:35 +0200

    Delete Practica3.1 directory

--- Practica3.1/includes/comun/pie.php
+++
@@ -1,6 +0,0 @@
-<footer class='foot'>
-        <p class='p-foot'>Síguenos</p>
-        <a href=""><img class='icon' src="./img/icon-twitter.png" alt=""></a>
-        <a href=""><img class='icon'src="./img/icon-facebook.png" alt=""></a>
-		<a href="#" class="scrollToTop" style="display: block;"><img src="./img/icon-arrow-top.png" alt="">
-</footer>
--- Practica3.1/index.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-require_once __DIR__.'/Explorar.php';
-
-
-require __DIR__.'/includes/plantillas/plantilla.php';
